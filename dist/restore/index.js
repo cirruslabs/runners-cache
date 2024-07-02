c803bdb73434a6c0fa873fa7bf175128110b10ab
Cirrus Labs <support@cirruslabs.org>
2024-07-02 00:04:31 +0000

    Cirrus Labs modifications

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -1,3 +1,12 @@
+const httpCacheHost = process.env["CIRRUS_HTTP_CACHE_HOST"];
+
+if (httpCacheHost) {
+    const newActionsCacheURL = "http://" + httpCacheHost + "/";
+
+    console.log("Redefining the ACTIONS_CACHE_URL to " + newActionsCacheURL + " to make the cache faster...");
+
+    process.env["ACTIONS_CACHE_URL"] = newActionsCacheURL;
+}
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
